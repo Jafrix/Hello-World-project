8b5cd1698834b6dcc8c5f36015097a5b63863e23
Andrey Veselov <lifeforglory@gmail.com>
2025-04-15 00:45:15 +0300

    v 0.0.1

---
+++ client/src/app/App.tsx
@@ -0,0 +1,24 @@
+import { RouterProvider } from "react-router-dom";
+import "./App.css";
+import { router } from "./router/router";
+
+import { Provider } from "react-redux";
+import store from "./store/store";
+
+import { YMaps } from "@pbe/react-yandex-maps";
+
+
+function App() {
+  return (
+    
+    <YMaps>
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
+    </YMaps>
+   
+  
+  );
+}
+
+export default App;
